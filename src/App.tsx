b16eef5074fc80e27abc7d54951ccd5558e03d5a
mozxf <nandogustavo2001*gmail.com>
2023-09-13 20:55:46 -0300

    installed ui dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import { Button } from './components/ui/button';
 
 export function App() {
   return (
     <>
       <h1>hello world</h1>
+      <Button>Teste</Button>
     </>
   );
 }
